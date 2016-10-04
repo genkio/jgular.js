31d3ddb4c56c306d11f13c739c144c688732ca1c
j1wu <ji.wu@outlook.com>
2016-10-04 13:06:29 +0900

    simply log exceptions to the console

--- test/scope_spec.js
+++ test/scope_spec.js
@@ -223,4 +223,43 @@ describe('digest', function() {
     expect(scope.counter).toBe(1);
   });
 
+  it('catches exceptions in watch function and continues', function() {
+    scope.aValue = 'abc';
+    scope.counter = 0;
+
+    scope.$watch(
+      function(scope) { throw 'Error'; },
+      function(newValue, oldValue, scope) { }
+    );
+    scope.$watch(
+      function(scope) { return scope.aValue; },
+      function(newValue, oldValue, scope) {
+        scope.counter++;
+      }
+    );
+
+    scope.$digest();
+    expect(scope.counter).toBe(1);
+  });
+
+  it('catches exceptions in listener function and continues', function() {
+    scope.aValue = 'abc';
+    scope.counter = 0;
+
+    scope.$watch(
+      function(scope) { return scope.aValue; },
+      function(newValue, oldValue, scope) { 
+        throw 'Error';
+      }
+    );
+    scope.$watch(
+      function(scope) { return scope.aValue; },
+      function(newValue, oldValue, scope) {
+        scope.counter++;
+      }
+    );
+
+    scope.$digest();
+    expect(scope.counter).toBe(1);
+  });
 });

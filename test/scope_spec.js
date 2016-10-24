e9d45e8722d32d75b6bc97ed544de4b316d83c77
j1wu <ji.wu@outlook.com>
2016-10-24 15:47:57 +0900

    implemented apply

--- test/scope_spec.js
+++ test/scope_spec.js
@@ -347,3 +347,31 @@ describe('$eval', function() {
     expect(result).toBe(44);
   });
 });
+
+describe('$apply', function() {
+  var scope;
+
+  beforeEach(function() {
+    scope = new Scope();
+  });
+
+  it('executes the given function and starts the digest', function() {
+    scope.aValue = 'someValue';
+    scope.counter = 0;
+
+    scope.$watch(
+      function(scope) { return scope.aValue; },
+      function(newValue, oldValue, scope) {
+        scope.counter++;
+      }
+    );
+
+    scope.$digest();
+    expect(scope.counter).toBe(1);
+
+    scope.$apply(function() {
+      scope.aValue = 'someOtherValue';
+    });
+    expect(scope.counter).toBe(2);
+  });
+});
